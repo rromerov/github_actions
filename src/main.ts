2df191857f3b34ea82307939234e836744e9e0a9
Roberto Romero Vielma <romero117@outlook.es>
2023-08-03 12:36:04 -0600

    Add jsonplaceholder

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,28 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
+async function loadUsers(){
+  const response = await fetch(import.meta.env.VITE_APP_API);
+  console.log(import.meta.env.VITE_APP_API)
+  const data = await response.json();
+
+  const userList = document.createElement('div')
+  data.forEach((element: any) => {
+    userList.innerHTML += `
+    <div>
+      <h2>${element.name}</h2>
+      </div>
+    `
+  });
+  const users = document.querySelector<HTMLDivElement>('#users');
+  if (users){
+  users.appendChild(userList);
+  }
+}
+
+loadUsers()
+
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -18,6 +40,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
+    <div id="users"></div>
   </div>
 `
 
